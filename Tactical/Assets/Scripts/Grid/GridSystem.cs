02f3486972642093d8a85ddfef3ad06965fc2e0c
Rubick07 <evanjonatham07@gmail.com>
2025-04-22 16:58:20 +0700

    Part 1

---
+++ Tactical/Assets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private GridObject[,] gridObjectArray;
+    public GridSystem(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridObjectArray = new GridObject[width, height];
+
+        for(int x = 0; x< width; x++)
+        {
+            for(int y = 0; y< height; y++)
+            {
+                GridPosition gridPosition = new GridPosition(x, y);
+                gridObjectArray[x,y] =  new GridObject(this, gridPosition);
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(GridPosition gridPosition)
+    {
+        return new Vector3(gridPosition.x, 0, gridPosition.z) * cellSize;
+    }
+
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        return new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / cellSize),
+            Mathf.RoundToInt(worldPosition.z / cellSize)
+            );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for(int x = 0; x< width; x++)
+        {
+            for(int y = 0; y< height; y++)
+            {
+                GridPosition gridPosition = new GridPosition(x, y);
+                Transform debugTransform = GameObject.Instantiate(debugPrefab, GetWorldPosition(gridPosition), Quaternion.identity);
+                GridDebugObject gridDebugObject = debugTransform.GetComponent<GridDebugObject>();
+                gridDebugObject.SetGridObject(gridObjectArray[x,y]);
+            }
+        }
+    }
+
+    public GridObject GetGridObject(GridPosition gridPosition)
+    {
+        return gridObjectArray[gridPosition.x, gridPosition.z];
+    }
+
+}
+
+
---
+++ Tactical/Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+        Debug.Log(new GridPosition(5,7));
+    }
+
+    private void Update()
+    {
+        //Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
